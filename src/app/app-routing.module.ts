2746e25582e4d69fd4610666351def70423b8d65
Johnny <Jonathan.Peckover@qa.com>
2019-07-04 13:29:35 +0100

    update player form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PreferencesComponent } from './user/components/preferences/preferences.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile/preferences', component: PreferencesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PreferencesComponent } from './components/preferences/preferences.component';
 import { MatchResultsComponent } from './components/match-results/match-results.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -9,8 +10,12 @@ import { MatchResultsComponent } from './components/match-results/match-results.
   declarations: [
     PreferencesComponent,
     MatchResultsComponent],
+  exports: [
+    PreferencesComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
 export class UserModule { }
