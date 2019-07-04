59c0a0fa5866ee413ce91d62573bd0184ea72ce1
Johnny <Jonathan.PeckoverQA@academytrainee.com>
2019-07-04 18:05:48 +0100

    basic set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
+import { MatchModule } from './match/match.module';
+import { AuthModule } from './auth/auth.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    UserModule,
+    AuthModule,
+    MatchModule,
+    UserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user/components/match-results/match-results.component.ts
+++ src/app/user/components/match-results/match-results.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-match-results',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MatchResultsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private user: UserService,
+  ) { }
 
   ngOnInit() {
   }
