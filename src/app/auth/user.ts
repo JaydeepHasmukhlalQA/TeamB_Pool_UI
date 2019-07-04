7da45ae1d04fad67951cb5aa2605869baf363bbb
Hugh <hughfharding@gmail.com>
2019-07-04 16:52:34 +0100

    updated user interface

--- src/app/auth/user.ts
+++ src/app/auth/user.ts
@@ -1,6 +1,13 @@
 export interface IUser {
+
     username: string;
     password: string;
     firstname: string;
     lastname: string;
+    games_played: string;
+    wins: number;
+    loss: number;
+    win_rate: number;
+    loss_rate: number;
+    _v: string;   
 }
