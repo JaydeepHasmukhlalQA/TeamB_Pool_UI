3fbf2a92af458ef7c13f5be324b4ec42a7cf7d63
Johnny <Jonathan.Peckover@qa.com>
2019-07-04 13:41:46 +0100

    merge develper into preferences3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { HttpClientModule } from '@angular/common/http';
 import { UserModule } from './user/user.module';
 import { ReactiveFormsModule } from '@angular/forms';
-=======
-import { UserModule } from './user/user.module';
->>>>>>> dab82479f0b74371e1f0914d8e4f335e6b406a58
 
 @NgModule({
   declarations: [
@@ -19,15 +15,12 @@ import { UserModule } from './user/user.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     HttpClientModule,
     UserModule,
-    ReactiveFormsModule
-=======
+    ReactiveFormsModule,
     AuthModule,
     MatchModule,
     UserModule,
->>>>>>> dab82479f0b74371e1f0914d8e4f335e6b406a58
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,23 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PreferencesComponent } from './components/preferences/preferences.component';
 import { MatchResultsComponent } from './components/match-results/match-results.component';
-<<<<<<< HEAD
 import { ReactiveFormsModule } from '@angular/forms';
-=======
 import { StatisticsComponent } from './components/statistics/statistics.component';
 import { ProfileComponent } from './components/profile/profile.component';
->>>>>>> dab82479f0b74371e1f0914d8e4f335e6b406a58
 
 
 
 @NgModule({
   declarations: [
     PreferencesComponent,
-<<<<<<< HEAD
-    MatchResultsComponent],
-  exports: [
-    PreferencesComponent
-=======
     MatchResultsComponent,
     StatisticsComponent,
     ProfileComponent
@@ -28,7 +20,6 @@ import { ProfileComponent } from './components/profile/profile.component';
     PreferencesComponent,
     StatisticsComponent,
     ProfileComponent
->>>>>>> dab82479f0b74371e1f0914d8e4f335e6b406a58
   ],
   imports: [
     CommonModule,
