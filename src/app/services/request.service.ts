a5f4efcfd4977efe3c73add05c46f673897e46c3
Ferdinand-Oluwaseye <ronferd2012@gmail.com>
2019-07-05 13:31:42 +0100

    linked Api with Ui on statistics

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,21 +1,54 @@
-    
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient
+    ) { }
 
-  //type T, as type will be passed down from [module].service
-  get<T>(url: string): Observable<T> {
+  get<T>(
+    url: string,
+    queryParams?: HttpParams | { [param: string]: string | string[] }
+  ): Observable<T> {
     return this.http.get<T>(url, {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
+      ...httpOptions,
+      params: queryParams
     })
   }
+
+  //POST request
+
+  post<T>(url:string, body:any, queryParams?: HttpParams | { [param: string]: string | string[] }): Observable<T> {
+    return this.http.post<T>(url, body, {
+      ...httpOptions,
+      params: queryParams
+    });
+  
+ }
+
+
+ delete<T>(url:string, queryParams?: HttpParams | { [param: string]: string | string[]}): Observable<T>{
+    return this.http.delete<T>(url,{
+      ...httpOptions,
+      params: queryParams});
+ }
+
+
+ put<T>(url:string, body:any, queryParams?: HttpParams | { [param: string]: string | string[]}): Observable<T>{
+  return this.http.put<T>(url, body, {
+    ...httpOptions,
+    params: queryParams});
+}
+
+
+
+
 }
