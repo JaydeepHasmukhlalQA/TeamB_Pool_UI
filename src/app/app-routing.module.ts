7fc73c7e32887ca140f293fe1eec8ef2db635331
Johnny <Jonathan.PeckoverQA@academytrainee.com>
2019-07-05 11:48:00 +0100

    table basics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
+import { RegisterComponent } from './auth/components/register/register.component';
+import { LoginComponent } from './auth/components/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PreferencesComponent } from './user/components/preferences/preferences.component';
+import { MatchResultsComponent } from './user/components/match-results/match-results.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'profile/preferences', component: PreferencesComponent},
+  {path: 'profile/match-results', component: MatchResultsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
